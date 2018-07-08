7981043e9fb8d3900856a5f1dab48a36ae523638
coord.e <me@coord-e.com>
2018-07-08 14:18:26 +0900

    Change: Enable to specify target to lint/test

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-docker-compose -f lint/docker-compose.yml run --rm frontend lint
-docker-compose -f lint/docker-compose.yml run --rm backend lint
+if [ "$#" -ne 0 ]; then
+  docker-compose -f lint/docker-compose.yml run --rm $@ lint
+else
+  docker-compose -f lint/docker-compose.yml run --rm frontend lint
+  docker-compose -f lint/docker-compose.yml run --rm backend lint
+fi
--- scripts/unit_test.sh
+++ scripts/unit_test.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-docker-compose -f unit_test/docker-compose.yml run --rm frontend
-docker-compose -f unit_test/docker-compose.yml run --rm backend
+if [ "$#" -ne 0 ]; then
+  docker-compose -f unit_test/docker-compose.yml run --rm $@
+else
+  docker-compose -f unit_test/docker-compose.yml run --rm frontend
+  docker-compose -f unit_test/docker-compose.yml run --rm backend
+fi
