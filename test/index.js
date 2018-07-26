3d22bf96ae255f27e0aaeccc27e828864b0bab32
coord.e <me@coord-e.com>
2018-07-26 17:17:19 +0900

    Add: Add authenication test

--- test/index.js
+++ test/index.js
@@ -20,4 +20,21 @@ describe('Login', function () {
     $('[data-test="home-login"]').click()
     expect(browser.getUrl()).to.equal('http://frontend:8000/lottery/login')
   })
+
+  it('Can successfully authenicate with example1', function () {
+    browser.url('http://frontend:8000/lottery/login')
+    browser.waitForVisible('iframe', 3000)
+    $('[data-test="loginview-username"]').setValue('example1')
+    browser.frame($('iframe').value)
+    $('.recaptcha-checkbox').click()
+    browser.waitForVisible('.recaptcha-checkbox-checkmark', 5000)
+    browser.frameParent()
+    $('[data-test="loginview-login"]').click()
+    setTimeout(() => {
+      browser.refresh()
+      browser.waitForVisible('h1', 3000)
+      const title = $('h1').getText()
+      expect(title).to.equal('Logged in as example1')
+    }, 1000)
+  })
 })
